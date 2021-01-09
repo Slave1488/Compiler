accd2b3d683a86628d4b2f8522bd75bfebb3fc4d
Slave <>
2021-01-09 21:23:02 +0500

    add EOF Terminal

--- source.cpp
+++ source.cpp
@@ -11,151 +11,160 @@ using std::vector;
 using std::map;
 using std::set;
 
-enum Terminal
+namespace compiler
 {
-	FAIL = -1,
-	NUM,
-	PLUS,
-	MINUS
-};
-
-struct Token
-{
-	static const Token Fail;
-
-	const Terminal Tag;
-
-	explicit Token(char sumbol): Token(define(sumbol))
+	enum Terminal
 	{
-	}
-protected:
-	explicit Token(Terminal terminal): Tag(terminal)
+		FAIL = -1,
+		NUM,
+		PLUS,
+		MINUS,
+		EOF_
+	};
+
+	struct Token
 	{
-	}
-private:
-	Terminal define(char sumbol)
-	{
-		switch (sumbol)
+		static const Token Fail;
+		static const Token Eof;
+
+		const Terminal Tag;
+
+		explicit Token(char sumbol): Token(define(sumbol))
 		{
-		case '+':
-			return PLUS;
-		case '-':
-			return MINUS;
 		}
-		return FAIL;
-	}
-};
-
-const Token Token::Fail = Token(FAIL);
-
-struct Num: public Token
-{
-	const int Value;
-
-	Num(int value): Token(NUM), Value(value)
-	{
-	}
-};
-
-struct Word: public Token
-{
-	const std::string Lexeme;
-protected:
-	Word(Terminal terminal, std::string lexeme): Token(terminal), Lexeme(lexeme)
-	{
-	}
-};
-
-class Lexer
-{
-public:
-	Lexer(istream& is): is(is)
-	{
-		is >> peek;
-	}
-
-	Token scan()
-	{
-		while (PeekIsSpace())
+	protected:
+		explicit Token(const Terminal& terminal): Tag(terminal)
 		{
-			is >> peek;
 		}
-		if (PeekIsDigit())
+	private:
+		const Terminal define(const char& sumbol) const
 		{
-			int num = peek - '0';
-			is >> peek;
-			while (PeekIsDigit())
+			switch (sumbol)
 			{
-				num = 10 * num + peek - '0';
+			case '+':
+				return PLUS;
+			case '-':
+				return MINUS;
+			}
+			return FAIL;
+		}
+	};
+
+	const Token Token::Fail = Token(FAIL);
+	const Token Token::Eof = Token(EOF_);
+
+	struct Num: public Token
+	{
+		const int Value;
+
+		Num(int value): Token(NUM), Value(value)
+		{
+		}
+	};
+
+	struct Word: public Token
+	{
+		const std::string Lexeme;
+	protected:
+		Word(Terminal terminal, std::string lexeme): Token(terminal), Lexeme(lexeme)
+		{
+		}
+	};
+
+	class Lexer
+	{
+	public:
+		Lexer(istream& is): is(is)
+		{
+			is >> peek;
+		}
+
+		Token scan()
+		{
+			while (PeekIsSpace())
+			{
 				is >> peek;
 			}
-			return Num(num);
+			if (PeekIsDigit())
+			{
+				int num = peek - '0';
+				is >> peek;
+				while (PeekIsDigit())
+				{
+					num = 10 * num + peek - '0';
+					is >> peek;
+				}
+				return Num(num);
+			}
+			if (!EndOfFile())
+			{
+				char sumbol = peek;
+				is >> peek;
+				return Token(sumbol);
+			}
+			return Token::Eof;
 		}
-		if (PeekIsSumbol())
+	private:
+		istream& is;
+		char peek;
+		map<std::string, Word> WordTable;
+
+		bool PeekIsSpace() const
 		{
-			char sumbol = peek;
-			is >> peek;
-			return Token(sumbol);
+			return !EndOfFile() && peek == ' ';
 		}
-		return Token::Fail;
-	}
-private:
-	istream& is;
-	char peek;
-	map<std::string, Word> WordTable;
 
-	bool PeekIsSpace() const
+		bool PeekIsDigit() const
+		{
+			return !EndOfFile() && '0' <= peek && peek <= '9';
+		}
+
+		bool EndOfFile() const
+		{
+			return is.eof();
+		}
+	};
+
+	bool derive_terminal(vector<Token>& string, int& lookahead, Terminal terminal)
 	{
-		return PeekIsSumbol() && peek == ' ';
+		if (string[lookahead].Tag == terminal)
+		{
+			++lookahead;
+			return true;
+		}
+		return false;
 	}
 
-	bool PeekIsDigit() const
+	bool derive_digit(vector<Token>& string, int& lookahead)
 	{
-		return PeekIsSumbol() && '0' <= peek && peek <= '9';
+		return false;
 	}
 
-	bool PeekIsSumbol() const
+	bool derive_list(vector<Token>& string, int& lookahead)
 	{
-		return !is.eof();
+		return derive_digit(string, lookahead) &&
+			(	(	derive_terminal(string, lookahead, PLUS) ||
+					derive_terminal(string, lookahead, MINUS)) &&
+				derive_list(string, lookahead) ||
+				true);
 	}
-};
 
-bool derive_terminal(vector<Token>& string, int& lookahead, Terminal terminal)
-{
-	if (string[lookahead].Tag == terminal)
+	bool derive(vector<Token>& string)
 	{
-		++lookahead;
-		return true;
+		int lookahead = 0;
+		return derive_list(string, lookahead) && lookahead == string.size();
 	}
-	return false;
 }
 
-bool derive_digit(vector<Token>& string, int& lookahead)
-{
-	return false;
-}
-
-bool derive_list(vector<Token>& string, int& lookahead)
-{
-	return derive_digit(string, lookahead) &&
-		(	(	derive_terminal(string, lookahead, PLUS) ||
-				derive_terminal(string, lookahead, MINUS)) &&
-			derive_list(string, lookahead) ||
-			true);
-}
-
-bool derive(vector<Token>& string)
-{
-	int lookahead = 0;
-	return derive_list(string, lookahead) && lookahead == string.size();
-}
+using namespace compiler;
 
 int main()
 {
 	ifstream ifs;
 	ifs.open("source.my");
 	Lexer lexer(ifs);
-	int buf;
-	while((buf = lexer.scan().Tag) != -1)
+	Terminal buf;
+	while((buf = lexer.scan().Tag) != EOF_)
 		cout << buf << '\n';
+	cout << buf << '\n';
 }
